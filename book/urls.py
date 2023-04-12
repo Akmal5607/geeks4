b6a889c0bc38ba93e014ca94e7c5d11c3975541a
Akmal5607 <chuvachok3310@gmail.com>
2023-04-12 23:34:28 +0600

    'hw1'

---
+++ book/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from book.views import helloview, bookview
+
+urlpatterns = [
+    path('hello/', helloview, name='hello'),
+    path('book/', bookview, name='book'),
+]
+
+
